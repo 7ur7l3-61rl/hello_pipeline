30daf3d564254cc423581106439334fb08899e5f
Turtle Girl <7ur7l3.61rl@gmail.com>
2018-02-04 14:02:44 +0100

    Whitespace

--- main.cpp
+++ main.cpp
@@ -49,51 +49,51 @@ public:
   }
 
   void SetUpMessageHandler(RefPtr<Glib::MainLoop> & main_loop) {
-      cout << __FUNCTION__ << ": Add lambda slot for error or eos on bus" << endl;
-      pipeline->get_bus()->add_watch([main_loop] (const RefPtr<Bus>&, const RefPtr<Message>& message) {
-          Glib::ustring message_name = Enums::get_name(message->get_message_type());
-          cout << "Message Handler: Message received: " << message_name << endl;
-          switch (message->get_message_type())
-          {
-          case MessageType::MESSAGE_EOS:
-              cout << "Message Handler: Got " << message_name << ", stopping message loop" << endl;
-              main_loop->quit();
-              break;
-          case MessageType::MESSAGE_ERROR:
-              cout << "Message Handler: Got " << message_name << ", stopping message loop" << endl;
-              main_loop->quit();
-              break;
-          default:
-              break;
-          }
-          return true;
-      });
+    cout << __FUNCTION__ << ": Add lambda slot for error or eos on bus" << endl;
+    pipeline->get_bus()->add_watch([main_loop] (const RefPtr<Bus>&, const RefPtr<Message>& message) {
+      Glib::ustring message_name = Enums::get_name(message->get_message_type());
+      cout << "Message Handler: Message received: " << message_name << endl;
+      switch (message->get_message_type())
+      {
+        case MessageType::MESSAGE_EOS:
+          cout << "Message Handler: Got " << message_name << ", stopping message loop" << endl;
+          main_loop->quit();
+          break;
+        case MessageType::MESSAGE_ERROR:
+          cout << "Message Handler: Got " << message_name << ", stopping message loop" << endl;
+          main_loop->quit();
+          break;
+        default:
+          break;
+      }
+      return true;
+    });
   }
 
   void ListenForPads() {
-      cout << __FUNCTION__ << ": Add lambda slot for new pad on decodebin" << endl;
-      decodebin->signal_pad_added().connect([&] (const RefPtr<Pad>& pad) {
-          cout << "PadListener: Pad added to : " << decodebin->get_name() << endl;
-          cout << "PadListener: Pad added    : " << pad->get_name() << endl;
+    cout << __FUNCTION__ << ": Add lambda slot for new pad on decodebin" << endl;
+    decodebin->signal_pad_added().connect([&] (const RefPtr<Pad>& pad) {
+      cout << "PadListener: Pad added to : " << decodebin->get_name() << endl;
+      cout << "PadListener: Pad added    : " << pad->get_name() << endl;
 
-          PadLinkReturn ret = pad->link(sink->get_static_pad("sink"));
+      PadLinkReturn ret = pad->link(sink->get_static_pad("sink"));
 
-          if (ret != PadLinkReturn::PAD_LINK_OK)
-              cout << "PadListener: Pads could not be linked : " << ret << endl;
-          else
-              cout << "PadListener: Pads linked " << endl;
-      });
+      if (ret != PadLinkReturn::PAD_LINK_OK)
+        cout << "PadListener: Pads could not be linked : " << ret << endl;
+      else
+        cout << "PadListener: Pads linked " << endl;
+    });
 
   }
 
   void StartPipeline() {
-      cout << __FUNCTION__ << ": Set Pipeline State to Playing " << endl;
-      pipeline->set_state(State::STATE_PLAYING);
+    cout << __FUNCTION__ << ": Set Pipeline State to Playing " << endl;
+    pipeline->set_state(State::STATE_PLAYING);
   }
 
   void StopPipeline() {
-      cout << __FUNCTION__ << ": Set Pipeline State to Null " << endl;
-      pipeline->set_state(State::STATE_NULL);
+    cout << __FUNCTION__ << ": Set Pipeline State to Null " << endl;
+    pipeline->set_state(State::STATE_NULL);
   }
 
   RefPtr<Element> source;
@@ -112,29 +112,29 @@ int main(int argc, char *argv[])
   std::string fileName = argv[1];
   cout << __FUNCTION__ << ": Input file " << fileName << endl;
 
-    cout << __FUNCTION__ << ": Initialize GStreamer" << endl;
-    Gst::init(argc, argv);
+  cout << __FUNCTION__ << ": Initialize GStreamer" << endl;
+  Gst::init(argc, argv);
 
-    cout << __FUNCTION__ << ": Create Mainloop" << endl;
-    RefPtr<Glib::MainLoop> main_loop = Glib::MainLoop::create();
+  cout << __FUNCTION__ << ": Create Mainloop" << endl;
+  RefPtr<Glib::MainLoop> main_loop = Glib::MainLoop::create();
 
-    cout << __FUNCTION__ << ": Create PipelineContainer" << endl;
-    PipelineContainer pipeline_container;
+  cout << __FUNCTION__ << ": Create PipelineContainer" << endl;
+  PipelineContainer pipeline_container;
 
-    cout << __FUNCTION__ << ": Initialize PipelineContainer" << endl;
-    pipeline_container.Initialize();
+  cout << __FUNCTION__ << ": Initialize PipelineContainer" << endl;
+  pipeline_container.Initialize();
 
-    cout << __FUNCTION__ << ": Setup Message Handling" << endl;
-    pipeline_container.SetUpMessageHandler(main_loop);
+  cout << __FUNCTION__ << ": Setup Message Handling" << endl;
+  pipeline_container.SetUpMessageHandler(main_loop);
 
-    cout << __FUNCTION__ << ": Setup Listener for Pads on Decodebin" << endl;
-    pipeline_container.ListenForPads();
+  cout << __FUNCTION__ << ": Setup Listener for Pads on Decodebin" << endl;
+  pipeline_container.ListenForPads();
 
-    cout << __FUNCTION__ << ": Start Pipeline" << endl;
-    pipeline_container.StartPipeline();
+  cout << __FUNCTION__ << ": Start Pipeline" << endl;
+  pipeline_container.StartPipeline();
 
   auto sink_reader_handle = async(launch::async, [&] () {
-        cout << " Sink Reader started " << endl;
+    cout << " Sink Reader started " << endl;
 
     RefPtr<AppSink> appsink = appsink.cast_static(pipeline_container.sink);
 
@@ -152,16 +152,16 @@ int main(int argc, char *argv[])
         break;
       }
     }
-    });
+  });
 
   auto source_writer_handle = async(launch::async, [&] () {
     cout << " Source Writer: async handler started " << endl;
 
     ifstream pipeline_input (fileName, ios::in|ios::binary);
-        if (!pipeline_input.is_open()) {
-            cout << " Could not open input file " << endl;
-            return;
-        }
+    if (!pipeline_input.is_open()) {
+      cout << " Could not open input file " << endl;
+      return;
+    }
 
     size_t remaining_size = getFileSize(fileName);
 
@@ -190,15 +190,15 @@ int main(int argc, char *argv[])
     }
 
     flow = appsrc->end_of_stream();
-    });
+  });
 
-    cout << __FUNCTION__ << ": Start Message Loop" << endl;
-    main_loop->run();
+  cout << __FUNCTION__ << ": Start Message Loop" << endl;
+  main_loop->run();
 
-    source_writer_handle.get();
-    sink_reader_handle.get();
+  source_writer_handle.get();
+  sink_reader_handle.get();
 
-    cout << __FUNCTION__ << ": Stop Pipeline" << endl;
-    pipeline_container.StopPipeline();
+  cout << __FUNCTION__ << ": Stop Pipeline" << endl;
+  pipeline_container.StopPipeline();
   return EXIT_SUCCESS;
 }
